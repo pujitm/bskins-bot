009f04b413ee54d908917152462062e88190ad16
Darth Windu <pujitm2009@gmail.com>
2018-12-09 13:24:47 -0500

    added tests

--- src/test/index.test.ts
+++ src/test/index.test.ts
@@ -2,13 +2,23 @@ require('dotenv').config()
 
 import { hello } from '../hello-test';
 import { expect } from 'chai';
+import * as BitSkins from "bitskins"
 import 'mocha';
 
 describe('Hello function', () => {
 
-  it('should return hello world', () => {
+  it('test is alive', () => {
     const result = hello();
     expect(result).to.equal('Hello World!');
   });
 
 });
+
+describe('Bitskins REST API', () => {
+  it('is alive', async () => {
+    const api = new BitSkins.API(process.env.API_KEY, process.env.API_SECRET)
+    const balance = await api.getAccountBalance()
+    expect(balance).to.not.be.null
+    // console.log(balance)
+  })
+})
