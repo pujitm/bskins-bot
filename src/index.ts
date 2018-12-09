784f189ad892812b70d088084133b5fbb3bf0a86
Darth Windu <pujitm2009@gmail.com>
2018-12-08 23:12:06 -0500

    Made console output more declarative

--- src/index.ts
+++ src/index.ts
@@ -12,19 +12,29 @@ socket.on('connected', () => {
 socket.on('inventory_changes:listed', (item: InventoryChangesObject) => {
     console.log(colors.FgGreen, 'Item Listed!', colors.Reset)
     console.log(colors.Dim, JSON.stringify(item), colors.Reset)
+    console.log(colors.Bright,`${item.market_hash_name}\n$ ${item.price}`, colors.Reset)
 })
 
 socket.on('inventory_changes:delisted_or_sold', (item: InventoryChangesObject) => {
     console.log(colors.FgRed, 'Item Delisted or Sold!', colors.Reset)
     console.log(colors.Dim, JSON.stringify(item), colors.Reset)
+    console.log(colors.Bright,`${item.market_hash_name}\n$ ${item.price}`, colors.Reset)
 })
 
 socket.on('inventory_changes:price_changed', (item: InventoryChangesObject) => {
     console.log(colors.FgYellow, 'Price Changed!', colors.Reset)
     console.log(colors.Dim, JSON.stringify(item), colors.Reset)
+    console.log(colors.Bright,`${item.market_hash_name}\nPrice Change: ${(item.old_price - item.price).toFixed(2)}`, colors.Reset)
 })
 
 socket.on('inventory_changes:extra_info', (item: InventoryChangesObject) => {
     console.log(colors.FgBlue, 'Extra Info Received!', colors.Reset)
     console.log(colors.Dim, JSON.stringify(item), colors.Reset)
+    // JSON Pretty printing: https://stackoverflow.com/questions/4810841/how-can-i-pretty-print-json-using-javascript
+    console.log(
+        colors.Bright,
+        `${item.market_hash_name}\n
+            Extra Info: ${JSON.stringify(item.extra_info, null, 2)}\n
+            StickerInfo: ${JSON.stringify(item.sticker_info, null, 2)}`,
+        colors.Reset)
 })
