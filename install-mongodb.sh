7c31395c830d6881f011208962781c319dd41011
Darth Windu <pujitm2009@gmail.com>
2018-12-20 15:04:09 -0500

    added update echo message

--- install-mongodb.sh
+++ install-mongodb.sh
@@ -9,6 +9,8 @@ echo "Setting up MongoDB repo..."
 
 apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 9DA31620334BD75D9DCB49F368818C72E52529D4
 echo "deb [ arch=amd64 ] https://repo.mongodb.org/apt/ubuntu bionic/mongodb-org/4.0 multiverse" | tee /etc/apt/sources.list.d/mongodb-org-4.0.list
+
+echo "Updating local packages..."
 apt-get update
 
 echo "Installing MongoDB..."
