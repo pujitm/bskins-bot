81ef40868f89014c691cd191529d1470716ec25a
Pujit Mehrotra <pujitm2009@gmail.com>
2019-07-28 20:59:22 -0400

    Add progress bar to brute stat update

--- src/cron/brute_stat_eval.js
+++ src/cron/brute_stat_eval.js
@@ -1,16 +1,24 @@
 require("dotenv").config(); // Should be first line application; loads environment variables
 const { Firestore } = require('@google-cloud/firestore');
 
+const _cliProgress = require('cli-progress');
+ 
+// create a new progress bar instance and use shades_classic theme
+const bar1 = new _cliProgress.Bar({}, _cliProgress.Presets.shades_classic);
+
 const firestore = new Firestore();
 const average = arr => arr.reduce((sum, element) => sum + element, 0) / arr.length;
 let prices = {};
 
 firestore.listCollections().then(async (collections) => {
+    bar1.start(collections.length, 0);
     for (let collection of collections) {
         // console.log(`Found collection with id: ${collection.id}`);
         let collPrices = await parseCollection(collection);
         prices[collection.id] = collPrices;
+        bar1.increment();
     }
+    bar1.stop();
     console.log(prices);
 });
 
